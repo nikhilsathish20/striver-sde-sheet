2485c8ab2f8d9ed92cbdae28227ad409bef2f5e1
Nikhil S <nikhilsathish20@gmail.com>
2026-06-09 02:17:01 +0530

    Added Day 8

---
+++ Arrays Part - IV/Count subarrays with given xor K/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int subarraysWithXorK(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int xr = 0;
+        int count = 0;
+
+        map.put(0, 1);
+
+        for (int num : nums) {
+            xr ^= num;
+
+            int x = xr ^ k;
+
+            count += map.getOrDefault(x, 0);
+
+            map.put(xr, map.getOrDefault(xr, 0) + 1);
+        }
+
+        return count;
+    }
+}
